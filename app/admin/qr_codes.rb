f2471d92e2f563cffbd34aa29d904af60f5308ca
Peter O'Leary <pete@timelight.com>
2020-06-17 13:50:11 -0700

    Add admin pages

---
+++ app/admin/qr_codes.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register QrCode do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :shortened_url, :shortened_url_id, :url, :title, :description, :qr_code_svg
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :shortened_url, :shortened_url_id, :url, :title, :description, :qr_code_svg]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
